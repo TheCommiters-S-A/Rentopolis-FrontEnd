f44215a767b0ebf92c3d1083566fd6638052070e
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-02-22 19:15:17 -0500

    Vista de bienvenida

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export const Login = (props) => {
-    return(
-        
-    )
+export const Bienvenida = (props) => {
+    return (
+
+        <div>
+            <div id="preloader"></div>
+
+            <section id="hero">
+                <div className="hero-container">
+                    <div className="fadeIn">
+                        <div className="hero-logo">
+                            <img className="" src="img/logo.png" alt="Imperial"/>
+                        </div>
+
+                        <br/>
+                            <div className="actions">
+                                <a href="/inicio" className="btn-get-started">Iniciar Sesión</a>
+                                <a href="/inicio" className="btn-get-started">Registrarse</a>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+)
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import GuardRoute from './GuardRoute';
+import {VistaPrincipal} from '../components/VistaPrincipal';
+import {Bienvenida} from '../components/Bienvenida';
 
 class Routes extends React.Component {
     render() {
         return (
-            <Switch>
-                <GuardRoute path="/inicio" type="private" component={VistaPrincipal}></GuardRoute>
-                <Route path="/" type="public" component={Bienvenida}></Route>
-            </Switch>
+            <BrowserRouter>
+                <Switch>
+                    <GuardRoute path="/inicio" type="private" component={VistaPrincipal}></GuardRoute>
+                    <Route path="/" type="public" component={Bienvenida}></Route>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
