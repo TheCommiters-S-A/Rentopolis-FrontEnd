c2b39214c382c0813b0ff1e47db5934f16e6e80b
Davor Cortes <davorcortes.dc@gmail.com>
2021-02-25 16:21:25 -0500

    Login UI

---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import { Button, Grid, TextField, FormControl, InputLabel, Input, InputAdornment, IconButton } from '@material-ui/core';
+import React, {useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import House from '../views/Login/casa.svg';
+import Icon from '../views/Login/icon.svg';
+import 'fontsource-roboto';
+import {LoginForm} from './LoginForm';
+
+
+
+const useStyle = makeStyles({
+    houseContainer:{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: `url(${House}) no-repeat center`,
+        backgroundSize: "contain",
+        height: "400px",
+        position: "relative",
+    },
+
+    icon:{
+        background: `url(${Icon}) no-repeat center`,
+        backgroundSize: "contain",
+        position: "relative",
+        height: "80px",
+        width: "80px",
+
+    }
+});
+
+export const Login = props =>{
+
+    const classes = useStyle();
+
+    return(
+        <div>
+            <div id="preloader"></div>
+            <section id="hero">
+                <div className="hero-container">
+                    <div className={classes.houseContainer}>
+                        <Grid container 
+                              direction="column"
+                              justify="center"
+                              alignItems="center"
+                              spacing={0}
+                        >
+                            <Grid item >
+                                <div className={classes.icon}>
+                                </div>
+                            </Grid>
+                            <Grid item>
+                                <LoginForm/>
+                            </Grid>
+                        </Grid>
+                     </div>  
+                </div>
+            </section>
+        </div>
+    );
+
+
+};
