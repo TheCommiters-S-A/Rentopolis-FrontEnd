2da833c4fa529492a3bdbda89656185fa1736937
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-03-10 18:22:36 -0500

    Acomodando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
-
 import {Buscar} from './Buscar'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Link from "@material-ui/core/Link";
+import {Grid} from "@material-ui/core";
+import React from "react";
 
 export const Header = (props) => {
     return (
         <section className="header-main border-bottom">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-lg-2 col-4">
+                    <div className="tituloo col-4">
                         <a style={{fontSize: 30}} href="/inicio" className="brand-wrap">
                             Rentopolis
                         </a>
@@ -16,7 +18,13 @@ export const Header = (props) => {
                     </div>
                     <Buscar></Buscar>
 
-                    <div className="col-lg-4 col-sm-6 col-12">
+                    <div className="col-lg-2 ">
+                        <Link align="center" href="/publicar">
+                            Publica tu inmueble
+                        </Link>
+                    </div>
+
+                    <div className="col-lg-2 col-sm-6 col-12">
                         <div className="widgets-wrap float-md-right">
 
                             <div className="widget-header">
