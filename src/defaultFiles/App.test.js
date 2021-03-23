b2b16be11056cc9019c205ef72860edcca8c6482
D4v0r <davorcortes.dc@gmail.com>
2021-03-23 17:49:00 -0500

    :heavy_check_mark:

--- src/defaultFiles/App.test.js
+++ src/defaultFiles/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
+import  { toBeInTheDocument } from '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
+test('renders initial page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/iniciar sesión/i);
+  expect(element).toBeInTheDocument();
 });
