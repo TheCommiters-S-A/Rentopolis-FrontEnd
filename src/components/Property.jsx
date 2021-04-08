a4bd0065b8b63684cd39bb4c0156594979f2e194
Carlos Gomez <gemec98@gmail.com>
2021-04-08 18:25:39 -0500

    build

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,48 +1,29 @@
 import React from 'react';
 
-
 export const Property = (props) => {
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
 
-
-    const items = [{
-        "name": "Apartamento en arriendo, SUBA, Bogotá D.C",
-        "price": 3402200,
-        "area": "2",
-        "numBathrooms": 4,
-        "numBedrooms": 3,
-        "picture": "assets/images/items/apto3.jpg"
-    }, {
-        "name": "Apartamento en arriendo, ENGATIVA, Bogotá D.C",
-        "price": 1231223,
-        "area": "2",
-        "numBathrooms": 2,
-        "numBedrooms": 3,
-        "picture": "assets/images/items/apto3.jpg"
-    },{
-        "name": "Apartamento en arriendo, CHAPINERO, Bogotá D.C",
-        "price": 3421000,
-        "area": "3",
-        "numBathrooms": 1,
-        "numBedrooms": 3,
-        "picture": "assets/images/items/apto3.jpg"
-    }
-    ];
+    let price = formatter.format(props.price);
+    let formatPrice = price.substring(0, price.length - 3);
 
     return (
         <div>
             <article className="card card-product-list">
                 <div className="row no-gutters">
                     <aside className="col-md-8">
-                        <img className="img-wrap" src={props.picture}/>
+                        <img alt="imagen" className="img-wrap" src={props.picture}/>
                         {/*<img className="img-wrap" src={props.picture}/>*/}
                     </aside>
                     <div className="col-md-3">
                         <div className="info-main" align="center">
-                            <a href="/#" className="h5 title"> {props.name} </a>
+                            <a href="/inmueble" className="h5 title"> {props.name} </a>
                             {/*<a href="#" className="h5 title"> Apartamento en arriendo, SUBA, Bogotá D.C </a>*/}
                             <br/>
                             <p className="text-black-50">Precio de arriendo </p>
-                            <span className="price h3"> $ {props.price} </span>
+                            <span className="price h3"> {formatPrice}</span>
                         </div>
                         <div className="row no-gutters">
 
