849be19015e1088b27bab7fc325f8027febe8fb9
Carlos Gomez <gemec98@gmail.com>
2021-04-08 17:21:47 -0500

    test

--- src/test/Profile.test.js
+++ src/test/Profile.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import {Profile} from "../components/profile/Profile";
+import '@testing-library/jest-dom/extend-expect';
 
-
-test('Header simple', () => {
+test('Profile Simple', () => {
     render(<Profile />);
     const linkElement = screen.getByText(/Nombre/i);
     expect(linkElement).toBeInTheDocument();
