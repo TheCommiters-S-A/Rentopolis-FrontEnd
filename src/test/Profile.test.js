66ed9f511ed70db894e97443e92e4e9e42efc945
Carlos Gomez <gemec98@gmail.com>
2021-04-08 20:31:14 -0500

    test

--- src/test/Profile.test.js
+++ src/test/Profile.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,cleanup } from '@testing-library/react';
 import {Profile} from "../components/profile/Profile";
 import{Property} from "../components/Property";
 import{VistaPrincipal} from "../components/VistaPrincipal";
 import '@testing-library/jest-dom/extend-expect';
+import mockedAxios from 'axios';
+
+
+afterEach(cleanup);
+test('mocking axios request', async () => {
+  const data = {
+  data: [
+   {
+     id: 1,
+     title: 'title 1'
+   },
+   {
+     id: 2,
+     title: 'title 2'
+   },
+   {
+     id: 3,
+     title: 'mocked title'
+  }]
+  };
+  mockedAxios.get;
+});
 
 test('Profile Simple', () => {
   render(<Profile />);
