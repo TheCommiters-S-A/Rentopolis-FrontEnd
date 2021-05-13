e134d85f0c2448fb3a30ccc5eb9e16a3c0c49ea1
Carlos Gomez <gemec98@gmail.com>
2021-05-12 21:55:16 -0500

    Delete users with firebase and service

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -172,54 +172,39 @@ export const Filter = (props) => {
                         <div className="card-body">
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Apartamento"/>
-                                <div className="custom-control-label">Apartamentos
-                                    <b className="badge badge-pill badge-light float-right">120</b>
-                                </div>
+                                <div className="custom-control-label">Apartamentos</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Apartaestudio"/>
-                                <div className="custom-control-label" >Apartaestudios
-                                    <b className="badge badge-pill badge-light float-right">15</b></div>
+                                <div className="custom-control-label" >Apartaestudios</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Casa"/>
-                                <div className="custom-control-label">Casas
-                                    <b className="badge badge-pill badge-light float-right">35</b></div>
+                                <div className="custom-control-label">Casas</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Oficina"/>
-                                <div className="custom-control-label">Oficinas
-                                    <b className="badge badge-pill badge-light float-right">89</b></div>
+                                <div className="custom-control-label">Oficinas</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Local"/>
-                                <div className="custom-control-label" >Locales
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
+                                <div className="custom-control-label" >Locales</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Bodega"/>
-                                <div className="custom-control-label">Bodegas
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
+                                <div className="custom-control-label">Bodegas</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Lote"/>
-                                <div className="custom-control-label" >Lotes
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
+                                <div className="custom-control-label" >Lotes</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Finca"/>
-                                <div className="custom-control-label">Fincas
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
-                            </label>
-                            <label className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Edificio"/>
-                                <div className="custom-control-label">Edificio de oficinas
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
+                                <div className="custom-control-label">Fincas</div>
                             </label>
                             <label className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" onChange={handletypeChange} value="Consultorio"/>
-                                <div className="custom-control-label" >Consultorios
-                                    <b className="badge badge-pill badge-light float-right">30</b></div>
+                                <div className="custom-control-label" >Consultorios</div>
                             </label>
                         </div>
                     </div>
